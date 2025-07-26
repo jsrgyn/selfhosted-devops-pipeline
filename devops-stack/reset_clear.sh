2b89246f19ad1d7a4f9f9758eba9e79590da316c
Johnathan Silva Resende <johnathansilvaresende@MacBook-Air-de-Johnathan.local>
2025-07-25 23:18:20 -0300

    Ajustes de projeto ok

--- devops-stack/reset_clear.sh
+++ devops-stack/reset_clear.sh
@@ -6,4 +6,5 @@ docker volume ls
 docker-compose down --volumes --remove-orphans
 ./clear_data.sh
 chmod +x config/postgres/init-scripts/01-init-databases.sh
+yes y | ssh-keygen -t rsa -b 4096 -f ./secrets/ssh/id_rsa -q -N ''
 docker-compose --env-file .env up -d --build --force-recreate
